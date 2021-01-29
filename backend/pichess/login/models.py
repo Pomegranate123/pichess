36c1254174a820545f6171c21394e713e266b69a
IzzyGiessen <i.vandergiessen@ziggo.nl>
2021-01-29 21:40:57 +0100

    backend2

--- backend/pichess/login/models.py
+++ backend/pichess/login/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
-class Player(AbstractUser):
+
+class Player(models.Model):
     rating = models.IntegerField()
 
 
