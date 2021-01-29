b68efae2ca08631a920ebaccb6b39f697c813cf2
Pomegranate123 <elias@hoste.info>
2021-01-29 19:57:33 +0100

    add .gitignore

---
+++ frontend/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <chessboard></chessboard>
+</template>
+
+<script>
+import chessboard from './components/Chessboard.vue'
+
+export default {
+  name: 'App',
+  components: {
+    chessboard
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
