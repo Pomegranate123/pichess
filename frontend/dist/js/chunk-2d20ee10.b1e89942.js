f0e357bd5d304072fa44b75ec2dcd0a7f15a5a37
Pomegranate123 <59014624+Pomegranate123@users.noreply.github.com>
2021-03-12 22:27:33 +0100

    Delete frontend/dist directory

--- frontend/dist/js/chunk-2d20ee10.b1e89942.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d20ee10"],{b0f2:function(n,e,c){"use strict";function o(n,e){return" Help "}c.r(e);const t={};t.render=o;e["default"]=t}}]);
-//# sourceMappingURL=chunk-2d20ee10.b1e89942.js.map
