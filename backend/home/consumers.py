7766be28c20b70476697323a925895180c925991
IzzyGiessen <i.vandergiessen@ziggo.nl>
2021-03-16 20:09:37 +0100

    Online players

--- backend/home/consumers.py
+++ backend/home/consumers.py
@@ -4,6 +4,7 @@ from channels.layers import get_channel_layer
 from django.contrib.auth import get_user_model
 from channels.consumer import AsyncConsumer
 from channels.db import database_sync_to_async
+from datetime import timedelta
 
 class LobbyConsumer(AsyncConsumer):
     async def websocket_connect(self, event):
