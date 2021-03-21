98c1e3eb38ffdb877accdf285fbb38cbd7a05b37
IzzyGiessen <i.vandergiessen@ziggo.nl>
2021-03-21 14:59:37 +0100

    .

--- backend/home/views.py
+++ backend/home/views.py
@@ -29,8 +29,8 @@ def profile(request):
         }
         return JsonResponse(data)
 
-def get_rating(request, username):
-    username = username['username']
+def get_rating(request):
+    username = request.headers['username']
     rating = User.objects.filter(username=username).values("email")[0]['email']
     data = {
         'rating': rating
